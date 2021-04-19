33012daad0aee85cc804820d753698c4badcbcec
peddivarunkumar <54740205+peddivarunkumar@users.noreply.github.com>
2021-04-19 18:34:54 +0530

    Delete tempCodeRunnerFile.c

--- tempCodeRunnerFile.c
+++
@@ -1,6 +0,0 @@
-void keysetup(grain* mygrain,const int* key,int keysize,int ivsize);
-void ivsetup(grain* mygrain,const int* iv);
-void keystream_bytes(grain* mygrain,int* keystream,int length);
-void encrypt_bytes(grain* mygrain,const int* plaintext,int* ciphertext,int msglen);
-void decrypt_bytes(grain* mygrain,const int* ciphertext,int* plaintext,int msglen);
-int grain_keystream(grain* mygrain);
